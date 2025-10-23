0ebd02547ce3cd583c1a1eb5319aafbe487b113a
tigattack <10629864+tigattack@users.noreply.github.com>
2025-10-23 18:44:27 +0100

    refactor(molecule/run.sh): set role name dynamically

--- molecule/run.sh
+++ molecule/run.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-role_symlink_dir='./tigattack.dawarich'
+namespace=`yq eval '.galaxy_info.namespace' meta/main.yml`
+role_name=`yq eval '.galaxy_info.role_name' meta/main.yml`
+
+role_symlink_dir="./${namespace}.${role_name}"
 
 if [ ! -L "$role_symlink_dir" ]; then
   echo "Creating symlinked role directory for test: $role_symlink_dir"
