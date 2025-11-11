0c7b18f16dc82ba2c92b5dcc3d2ea2a64f7d480f
tigattack <10629864+tigattack@users.noreply.github.com>
2025-11-11 21:43:54 +0000

    chore(patch_version): stage README if changed

--- .github/patch_version.sh
+++ .github/patch_version.sh
@@ -9,3 +9,10 @@ else
 fi
 
 $sedcmd -i -E "s/^(> \*\*Supported Dawarich version\(s\):\*\* [^0-9]+)([0-9]+\.[0-9]+(\.[0-9]+)?)/\1${new_version}/g" README.md
+
+if `git diff --quiet README.md`; then
+  echo "No changes made to README.md"
+else
+  echo "Updated supported Dawarich version to ${new_version} in README.md."
+  git add README.md
+fi
